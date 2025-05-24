f85c82b30bd912b8a59f30249bd41c6c4851c8f6
Menycoh <domenicoditella5@gmail.com>
2025-05-24 16:35:43 +0200

    Aggiunta esercizio calcolo media

---
+++ day_02/CalcoloMedia.java
@@ -0,0 +1,22 @@
+package day_02;
+import java.util.Scanner;
+public class CalcoloMedia {
+
+	public static void main(String[] args) {
+	int a,b,c,media;
+	
+	Scanner input = new Scanner (System.in);
+	System.out.println("Inserisci 3 numeri: ");
+	
+	a = input.nextInt();
+	b = input.nextInt();
+	c = input.nextInt();
+	
+	media = (a + b + c) / 3;
+	
+	System.out.println("Il risultato è: "+ media);
+	
+	input.close();
+	}
+
+}
